12c806df0d26d43d75eedf20b11a674ca746949d
dinkar1213 <58113960+dinkar1213@users.noreply.github.com>
2021-10-19 09:08:50 +0530

    Delete Banking directory

--- Banking/src/main/java/com/lti/entity/AccountType.java
+++
@@ -1,5 +0,0 @@
-package com.lti.entity;
-
-public enum AccountType {
-  SAVINGS,CURRENT,DEMAT
-}
--- Banking/src/main/java/com/lti/entity/ApprovalStatus.java
+++
@@ -1,7 +0,0 @@
-package com.lti.entity;
-
-
-public enum ApprovalStatus {
- APPROVED,REJECTED,APPLIED
- 
-}
